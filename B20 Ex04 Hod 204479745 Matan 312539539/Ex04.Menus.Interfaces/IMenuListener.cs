0c1f598c47d879aa293fa496a2130549d7d6a2f3
Hodjy <62711261+Hodjy@users.noreply.github.com>
2020-06-04 18:43:25 +0300

    Testing Update

---
+++ B20 Ex04 Hod 204479745 Matan 312539539/Ex04.Menus.Interfaces/IMenuListener.cs
@@ -0,0 +1,7 @@
+﻿namespace Ex04.Menus.Interfaces
+{
+    interface IMenuListener
+    {
+        void Run(int i_Index);
+    }
+}
---
+++ B20 Ex04 Hod 204479745 Matan 312539539/Ex04.Menus.Interfaces/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    class Menu : IMenuListener
+    {
+        public Menu(List<IMenuListener> i_List)
+        {
+
+        }
+
+        public void Run(int i_Index)
+        {
+            // start menu operation
+        }
+
+        /*
+         userchosesomething:
+         5
+         list[5].Run();
+         
+         
+         --EXE
+
+        list <- menu2 menu3 item1 item4 item7
+         
+         */
+    }
+}
