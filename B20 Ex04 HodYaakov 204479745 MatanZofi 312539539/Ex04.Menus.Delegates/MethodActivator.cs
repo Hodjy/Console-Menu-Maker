1611d95ccc94ccb13eba36ba61923732b9671cc2
TheFlash1907 <matanz19@gmail.com>
2020-06-14 16:34:38 +0300

    done

---
+++ B20 Ex04 HodYaakov 204479745 MatanZofi 312539539/Ex04.Menus.Delegates/MethodActivator.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex04.Menus.Delegates
+{
+    using System;
+
+    public delegate void Activator();
+
+    internal class MethodActivator : ConsoleItem
+    {
+        public event Activator MethodClicked;
+
+        public MethodActivator(string i_Name)
+            : base(i_Name)
+        {
+        }
+
+        internal override void Run()
+        {
+            Console.Clear();
+            OnMethodClicked();
+            pressToContinue();
+        }
+
+        private void pressToContinue()
+        {
+            Console.WriteLine("Press ENTER to continue.");
+            Console.ReadLine();
+        }
+
+        protected virtual void OnMethodClicked()
+        {
+            MethodClicked.Invoke();
+        }
+    }
+}
---
+++ B20 Ex04 HodYaakov 204479745 MatanZofi 312539539/Ex04.Menus.Interfaces/MethodActivator.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex04.Menus.Interfaces
+{
+    using System;
+
+    internal class MethodActivator : ConsoleItem
+    {
+        private IMenuListener m_Activator;
+        private int m_MethodIndex;
+
+        public MethodActivator(IMenuListener Listener, string i_Name, int i_Index) 
+            : base(i_Name) 
+        {
+            m_Activator = Listener;
+            m_MethodIndex = i_Index;
+        }
+
+        internal override void Run()
+        {
+            Console.Clear();
+            m_Activator.ActivateMethod(m_MethodIndex);
+            pressToContinue();
+        }
+
+        private void pressToContinue()
+        {
+            Console.WriteLine("Press ENTER to continue.");
+            Console.ReadLine();
+        }
+    }
+}
